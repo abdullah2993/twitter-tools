40ef8eb27f22ae49df07f0ff90ade70701071135
Abdullah Saleem <a.saleem2993@gmail.com>
2023-03-26 00:36:38 +0500

    Add verbose mode and resume capability

--- cmd/search/main.go
+++ cmd/search/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"net/url"
 	"os"
 
@@ -22,6 +24,8 @@ func main() {
 	flag.Usage = usage
 
 	pretty := flag.Bool("pretty", true, "Format JSON")
+	resume := flag.Bool("resume", false, "Resume from last file")
+	verbose := flag.Bool("verbose", false, "Verbose output")
 
 	flag.Parse()
 	nargs := flag.NArg()
@@ -31,8 +35,47 @@ func main() {
 	}
 
 	api := anaconda.NewTwitterApiWithCredentials(shared.Config.AccessToken, shared.Config.AccessTokenSecret, shared.Config.ConsumerKey, shared.Config.ConsumerSecret)
+	if *verbose {
+		api.Log = anaconda.BasicLogger
+	}
 	v := url.Values{}
 	v.Add("count", "100")
+
+	if *resume {
+		f, err := os.OpenFile(flag.Arg(1), os.O_RDONLY, 0600)
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				fmt.Fprintf(os.Stderr, "unable to resume because file doesn't exist: %s", flag.Arg(1))
+				goto skip
+			} else {
+				shared.FailOnError(err, "unable to open previous result file: %s", flag.Arg(1))
+			}
+		}
+
+		dec := json.NewDecoder(f)
+		last := new(anaconda.Tweet)
+		count := 0
+		for {
+			err := dec.Decode(last)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				} else {
+					shared.FailOnError(err, "unable to get last entry from previous result file: %s", flag.Arg(1))
+				}
+			}
+			count++
+		}
+		err = f.Close()
+		if err != nil {
+			shared.FailOnError(err, "unable to close previous result file: %s", flag.Arg(1))
+		}
+		fmt.Printf("resuming after %d tweets at id: %s", count, last.IdStr)
+		v.Add("max_id", last.IdStr)
+	}
+
+skip:
+
 	res, err := api.GetSearch(flag.Arg(0), v)
 	shared.FailOnError(err, "unable to search via api")
 
